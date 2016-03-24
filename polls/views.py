915eb43df6218c34b5e70640bcc0b3947c507e9e
Wuang Qin <wuang@leohealth.com>
2016-03-23 20:50:21 -0400

    add the response in view.py

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at qestion %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("You are looking at the result of question %s.")
+
+def vote(request, question_id):
+	return HttpResponse("You are voting on quesiton %s." % question_id)
